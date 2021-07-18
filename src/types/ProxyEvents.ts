e12a4f1be38babc5a6829bd12839a6b1b209b175
zxnii <61130784+Zxnii@users.noreply.github.com>
2021-07-18 00:46:05 -0600

    add events for connection

--- src/types/ProxyEvents.ts
+++ src/types/ProxyEvents.ts
@@ -5,7 +5,9 @@ export interface ProxyEvents {
 	client_error: (err: Error) => void
 	remote_end: () => void
 	remote_error: (err: Error) => void
-	player_join: (uuid: string, username: string) => void
+	player_join: (uuid: string, username: string, bot?: boolean) => void
 	player_leave: (uuid: string, username: string) => void
+	connected_remote: () => void
+	connected_local: () => void
 	chat: (message: ChatMessage) => void
 }
